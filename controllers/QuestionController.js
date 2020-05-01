b70fe1c504bc8e6c06a90cd538b5e306591d21d7
Leonardo Luís Dalcegio <leoodalcegio@hotmail.com>
2020-04-30 21:14:18 -0300

    Update QuestionController.js

--- controllers/QuestionController.js
+++ controllers/QuestionController.js
@@ -1,18 +1,24 @@
+const synonyms = require('synonyms');
+
 module.exports = {
     async ask(request, response){
         const { question, productInfo } = request.body;
 
         let matches = { };
 
+        // palavras chaves da question;
+
+        // 
+
         productInfo.forEach(() => {
-            // Compara as palavras da question com os "itens" do product info
+            // loop no productInfo, para cada um faz o synom
             // Se deu match, coloca item e valor no objeto de matches
             
             // Se algum item for um array, ou tiver mais de um valor, ex: item1: { valor1, valor2 },
-            // coloca o "item": se tem nome, -> "nome: valor" , "nome: valor"
-                              //se não tem nome, valor, valor, valor
+            // coloca o "item": (se tem nome, -> "nome: valor" , "nome: valor"
+                              //se não tem nome, valor, valor, valor)
         }); 
 
-        // monta uma resposta bonita, devolve ela
+        // monta uma resposta "bonita", devolve ela
     }
 }
